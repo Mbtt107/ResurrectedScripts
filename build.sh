0bcc3211059fa9e599acacf0c401c8072b52a780
Mbtt <harddeath13@gmail.com>
2018-09-29 15:53:51 +0300

    Install maven if branch is cm-13.0
    Signed-off-by: Mbtt <harddeath13@gmail.com>

--- build.sh
+++ build.sh
@@ -23,9 +23,9 @@ sudo apt-get update
 
 echo "Installing Java Development Kit..."
 if [ "$branch" == lineage-15.1 ]; then
-    sudo apt-get update openjdk-8-jdk
+    sudo apt-get install openjdk-8-jdk
 else
-    sudo apt-get update openjdk-7-jdk
+    sudo apt-get install openjdk-7-jdk
 fi
 
 echo "Installing necessary packages..."
@@ -33,6 +33,11 @@ sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-
              gnupg gperf imagemagicklib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool \
              libncurses5-dev libsdl1.2-devlibssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop \
              pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
+echo "Installing Maven Package..."
+if [ "$branch" == cm-13.0 ]; then
+    sudo apt-get install maven
+fi
 
 mkdir -p ~/bin
 PATH=~/bin:$PATH
